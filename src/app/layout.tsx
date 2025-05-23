e0ed243c88c5813fccfe3ad63d431a3b5e22ba92
hachrisjordan <nguyen77@miamioh.edu>
2025-05-23 16:53:23 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="font-mono" suppressHydrationWarning>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider>
           <div className="relative flex min-h-screen flex-col">
             <Header />
             <main className="flex-1">{children}</main>
