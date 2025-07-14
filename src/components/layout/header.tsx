c399bbc2711c9fcf7ce947f72c09a87684e8f712
hachrisjordan <nguyen77@miamioh.edu>
2025-07-14 17:08:15 -0400

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -16,7 +16,6 @@ const navigation = [
   { name: 'Seat Type / Delay', href: '/seat-type-delay' },
   { name: 'Award Finder', href: '/award-finder' },
   { name: 'Live Search', href: '/live-search' },
-  { name: 'Find Airport', href: '/find-airport' },
 ];
 
 const specialAwards = [
